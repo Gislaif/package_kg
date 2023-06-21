86890659be20cdb43938f4ac852b3e01b2828aec
Gislaif <134959294+Gislaif@users.noreply.github.com>
2023-06-21 07:11:42 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,2 +1,10 @@
-a = 3
-if a % 2 == 1:
+x = int(input("Enter a number from 0 to 100: "))
+while x !=1:
+    if x>100:
+        print(f"{x} is upper 100. Please, type a number 0 to 100")
+    elif x % 2==0:
+        print(x/2)
+        x=x/2
+    else:
+        print((3*x)+1)
+        x=3*x+1
