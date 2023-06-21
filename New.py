3af878d4a644cd63b4a10eb0d07838952f9ed4a7
Fernandes <gislaif@amazon.com>
2023-06-21 21:48:15 +0100

    Packaging program

--- New.py
+++ New.py
@@ -1,5 +1,8 @@
 package_count=0
 package_weight=0
+total_weight=0
+package_number=0
+max_unused_capacity=0
 
 items=int(input("Type how many items you would like to ship: "))
 for item_number in range(1, items +1):
@@ -7,20 +10,40 @@ for item_number in range(1, items +1):
     item_weight = int(input())
     if item_weight==0:
         break
-    if item_weight>0 and item_weight<=20:
+    if item_weight>0 and item_weight<=10:
         package_weight=package_weight+item_weight
-        if package_weight>=20:
+        total_weight=item_weight+total_weight
+        if package_weight>20:
             print("The package is full")
             print("Creating a new package")
-            package_count = package_count+1
+            unused_capacity=20-package_weight+item_weight
+            print("unused capacity of this package is: ", unused_capacity)
+            package_count = package_count + 1
+            if unused_capacity>max_unused_capacity:
+                max_unused_capacity=unused_capacity
+                package_number=package_count
             package_weight= item_weight
+    else:
+        print("Item weight is not correct")
+        continue
 if package_weight>0 and package_weight<=20:
+    unused_capacity = 20 - package_weight
+    print("unused capacity of this package is: ", unused_capacity)
     package_count = package_count + 1
+    if unused_capacity > max_unused_capacity:
+        max_unused_capacity = unused_capacity
+        package_number = package_count
     package_weight = item_weight
     print("total package count:")
     print(package_count)
-    print("Kg per package")
-    print(item_weight+package_weight)
+    print("Total weight of packages")
+    print(total_weight)
+    print("Total unused capacity")
+    print(20*package_count-total_weight)
+    print("Package is the most used capacity:" , package_number)
+    print("The most unused capacity in package: ", max_unused_capacity)
+
+
 
 
 
